ca3512d705cb74972e6274db6ac11bb89dfebb2e
Yassine Rafa <yassine.rafa@outlook.com>
2016-07-12 12:06:14 +0100

    Modifie the view and put all elment inside a container

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160712095359) do
+ActiveRecord::Schema.define(version: 20160712105329) do
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -29,5 +29,7 @@ ActiveRecord::Schema.define(version: 20160712095359) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
